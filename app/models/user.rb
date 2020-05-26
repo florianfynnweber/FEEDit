e51afb2c019451f1cf406ca99293470b8bbc8a9c
florianfynnweber <florian.fynn.weber@gmail.com>
2020-05-27 00:09:28 +0200

    controller test

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+  validates :email, presence: true, length: {maximum: 256},
+            uniqueness: {case_sensitive: false}
+end
