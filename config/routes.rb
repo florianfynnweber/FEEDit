b5ebd260b0c350dd318cf15d8fe57bffc3e82373
florianfynnweber <florian.fynn.weber@gmail.com>
2020-05-27 00:32:59 +0200

    add devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', confirmations: 'users/confirmations', passwords: 'users/passwords', unlocks: 'users/unlocks' } do
+  end
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
 end
