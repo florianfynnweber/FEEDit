6410a0e1f0dbab40de5df6ffb262a913d92156be
florianfynnweber <florian.fynn.weber@gmail.com>
2020-05-27 00:53:09 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   end
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+    end
+  end
   root to: "pages#home"
 end
